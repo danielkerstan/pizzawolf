ae13274f1bc940cad82cc1685bfe6710079ecd92
danielkerstan <danielkerstan@bren.ucsb.edu>
2021-08-13 15:44:25 -0700

    added two more functions

---
+++ R/addition.R
@@ -0,0 +1,13 @@
+#' addition
+#'
+#' @param value_1 number of the first value
+#' @param value_2 number of the second value
+#'
+#' @return the sum of the two input numbers
+#' @export
+#'
+#' @examples addition(value_1 = 1, value_2 = 2)
+addition <- function(value_1, value_2) {
+  sum = value_1 + value_2
+  print(sum)
+}
---
+++ R/multiplication.R
@@ -0,0 +1,13 @@
+#' Multiplication
+#'
+#' @param value_1 first number input
+#' @param value_2 second number input
+#'
+#' @return product of two input numbers
+#' @export
+#'
+#' @examples multiplication(value_1 = 3, value_2 = 4)
+multiplication <- function(value_1, value_2) {
+  product = value_1 * value_2
+  print(product)
+}
